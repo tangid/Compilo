02c47019c05e4c676d64845b86badcc4ef6b90a2
tangid <33022427+tangid@users.noreply.github.com>
2018-06-14 10:05:12 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,22 @@
+int main(){
+	int i, j,k,r;
+	i =3;
+	j=4;
+	k=8;
+	printf (i);
+	r=(i+j)*(i+k/j);
+	printf ( r );
+	if (i<5)
+	{	
+		i = i + 5 ;
+	}
+	else
+	{
+		i = i+4;
+	}
+	while (i)
+	{
+		i = i + 1;
+	}
+
+}
